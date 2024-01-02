ee20a1874cc50b9fa285effd52ac7339ccd17ef2
Shelton Francisco <sheltonfjs@gmail.com>
2024-01-02 14:27:19 +0200

    feat: using Builder pattern on exceptions hanlder

--- src/main/java/sheltonfrancisco/geomozapi/exceptions/ExceptionDetails.java
+++ src/main/java/sheltonfrancisco/geomozapi/exceptions/ExceptionDetails.java
@@ -1,10 +1,12 @@
 package sheltonfrancisco.geomozapi.exceptions;
 
+import lombok.Builder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record ExceptionDetails (
         HttpStatusCode statusCode,
         HttpStatus httpStatus,
