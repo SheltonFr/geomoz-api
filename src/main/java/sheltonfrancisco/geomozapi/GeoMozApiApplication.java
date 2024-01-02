db0194377092399cca44fa3f1ebe59be3eac6b08
Shelton Francisco <ssalicuchepa@technoplus.co.mz>
2024-01-02 11:23:54 +0200

    first commit

---
+++ src/main/java/sheltonfrancisco/geomozapi/GeoMozApiApplication.java
@@ -0,0 +1,13 @@
+package sheltonfrancisco.geomozapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoMozApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoMozApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheltonfrancisco/geomozapi/GeoMozApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sheltonfrancisco.geomozapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoMozApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
